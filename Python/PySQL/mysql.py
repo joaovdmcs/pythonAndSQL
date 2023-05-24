40d8d4e24e1cb10a8cded4b3251693a77ba7e59e
joaovdmcs <joao.vitor.souza@ccc.ufcg.edu.br>
2023-05-24 18:38:16 -0300

    SQL-Python CRUD

--- Python/PySQL/mysql.py
+++ Python/PySQL/mysql.py
@@ -1,3 +1,4 @@
+#MySQL/MariaDB Connection.
 import MySQLdb
 host = "localhost"
 user = "aplicacao"
@@ -6,3 +7,20 @@ db = "Escola"
 port = 3306
 con = MySQLdb.connect(host,user,password,db,port)
 
+cur = con.cursor()
+#cur = con.cursor(MySQLdb.cursors.DictCursor) to return Dict.
+
+#CRUD Creation
+def select(fields, tables, where=None):
+	
+	global cur
+	
+	query = "SELECT " + fields + " FROM " + tables
+	if (where):
+		query = query + " WHERE " + where
+	
+	cur.execute(query)
+	return cur.fetchall() #Tupla.
+
+
+print(select("Nome, CPF", "Aluno"))
